37837d9f7f99d5b3cc4323db6fc8aa568012c915
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-29 16:40:26 -0500

    timer is working, useRef implemented and count working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
   const [text, setText] = useState("");
   const [count, setCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimerOn, setIsTimerOn] = useState(false);
+  const textAreaRef = useRef(null);
 
   function handleChange(event) {
     const { value } = event.target;
@@ -14,12 +17,34 @@ function App() {
     return wordsArr.filter(word => word !== "").length;
   }
 
+  function handleClick() {
+    setIsTimerOn(true);
+    setText("");
+    textAreaRef.current.disabled = false;
+    textAreaRef.current.focus();
+  }
+
+  function endGame() {
+    setIsTimerOn(false);
+    setCount(wordCount(text));
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && isTimerOn) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimerOn]);
+
   return (
     <main>
       <h1>This is a word game</h1>
-      <textarea value={text} onChange={handleChange} />
-      <h3>Time remaining: </h3>
-      <button type="submit" onClick={() => wordCount(text)}>Start the game</button>
+      <textarea disabled={!isTimerOn} value={text} onChange={handleChange} ref={textAreaRef} />
+      <h3>Time remaining: {timeRemaining} </h3>
+      <button disabled={isTimerOn} type="submit" onClick={handleClick}>Start the game</button>
       <h1>Number of words: {count} </h1>
     </main>
   );
