857111aab041764db09e3c1392e307c210402831
renaud archer fortin <renaud.archerf@gmail.com>
2020-12-29 15:57:38 -0500

    update depedencies in package.json

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(0);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setText(value);
+  }
+
   return (
     <main>
       <h1>This is a word game</h1>
-      <textarea />
+      <textarea onChange={handleChange} />
       <h3>Time remaining: </h3>
       <button type="submit">Start the game</button>
-      <h1>Number of words: </h1>
-
+      <h1>Number of words: {count} </h1>
     </main>
   );
 }
 
-/**
- * Challenge: build the basic structure of our game
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in 
- *      (tip: React normalizes <textarea /> to be more like <input />, 
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
 export default App;
